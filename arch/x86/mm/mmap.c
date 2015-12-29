7b89b840da1c74787883c2e1056b0a3b63cc2ee6
dcashman <dcashman@google.com>
2015-12-29 15:44:12 -0800

    FROMLIST: x86: mm: support ARCH_MMAP_RND_BITS.
    (cherry picked from commit https://lkml.org/lkml/2015/12/21/339)
    
    x86: arch_mmap_rnd() uses hard-coded values, 8 for 32-bit and 28 for
    64-bit, to generate the random offset for the mmap base address.
    This value represents a compromise between increased ASLR
    effectiveness and avoiding address-space fragmentation. Replace it
    with a Kconfig option, which is sensibly bounded, so that platform
    developers may choose where to place this compromise. Keep default
    values as new minimums.
    
    Bug: 24047224
    Signed-off-by: Daniel Cashman <dcashman@android.com>
    Signed-off-by: Daniel Cashman <dcashman@google.com>
    Change-Id: I4f0cb7159406166fbc9f8938ba8d3e24b72eeefb
    (cherry picked from commit aa35bbf35451475dfc7c31ce5f97975c5542bd2e)
    Mot-CRs-fixed: (CR)
    Reviewed-on: https://gerrit.mot.com/900440
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Zhi-Ming Yuan <a14194@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- arch/x86/mm/mmap.c
+++ arch/x86/mm/mmap.c
@@ -69,10 +69,6 @@ static unsigned long mmap_rnd(void)
 {
 	unsigned long rnd;
 
-	/*
-	 *  8 bits of randomness in 32bit mmaps, 20 address space bits
-	 * 28 bits of randomness in 64bit mmaps, 40 address space bits
-	 */
 	if (mmap_is_ia32())
 		rnd = (unsigned long)get_random_int() % (1<<8);
 	else
